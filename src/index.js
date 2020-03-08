a21693feb975ec5ef15dc017e25ea452e469e70d
Prokopenko <alex27prok@gmail.com>
2020-03-09 02:15:54 +0300

    fix: Add function, which find min, max, avg number in an array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array && array.length)
+  {
+    var temp = array.sort(function(a, b){return a-b});
+    return temp[0];
+  }
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array && array.length)
+  {
+    var temp = array.sort(function(a, b){return a-b});
+    return temp[array.length - 1];
+  }
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array && array.length)
+  {
+    var temp = 0;
+    for(var i = 0; i<array.length; i++)
+      temp += array[i];
+    return temp/array.length;
+  }
+  else return 0;
 }
